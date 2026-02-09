e7e5c44514382f44fd460ec57aa4ab442a5663f9
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2026-02-08 18:54:39 -0800

    bonks and drop screen

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 git pull origin master
 git submodule update --init --recursive
 dotnet build DiscordLevelsBot.sln --configuration Release -o ./bin/live_release
-screen -dmS levelsbot dotnet bin/live_release/DiscordLevelsBot.dll $1
+dotnet bin/live_release/DiscordLevelsBot.dll $1
